49a74eb3ad05451127a7e8915fcb5fcafa59f90e
Juan Manuel Berros <juanmaberros@gmail.com>
2016-11-24 16:19:02 -0300

    pull request fixes

--- anotamela/vcf_to_dataframe.py
+++ anotamela/vcf_to_dataframe.py
@@ -3,27 +3,24 @@ import re
 import vcf
 import gzip
 import pandas as pd
-from tqdm import tqdm
 
 from anotamela.helpers import make_chromosome_series_categorical
 
 
 GENO_REGEX = re.compile(r'([\d|\.](?:[/|\|][\d|\.])?)')
 
-def vcf_to_dataframe(vcf_path, keep_format_data=False, keep_samples=[]):
+def vcf_to_dataframe(vcf_path, keep_samples=[], keep_format_data=False):
     """
-    Generate a pandas.DataFrame of the variants present in a VCF. Removes
-    the duplicated rows. WARNING: It will hog RAM and take a while for big
-    VCFs.
+    Generates a pandas.DataFrame of the variants present in a VCF. Removes
+    the duplicated rows.
 
-    To avoid big time use of RAM and cycles, the default behavior is not to
-    read any of the genotypes. In case you want to keep the genotypes, set
-    keep_samples with a sample ID or list of sample IDs. In case you want
-    all the samples, set keep_samples='all' (WARNING: this will take loads of
-    RAM and time for big VCFs).
+    To avoid high consumption of RAM and cycles, the default behavior is not to
+    read any of the genotypes. In case you want to keep the genotypes, you have
+    to set keep_samples explicitely with a sample ID or a list of sample IDs.
 
-    If keep_format_data is set, it will keep the metadata for each
-    genotype call, e.g. AD, DP, GQ, PL, etc. If not, just the genotypes (GT).
+    If you set keep_samples and keep_format_data, it will keep the metadata for
+    each genotype call, e.g. AD, DP, GQ, etc. If not, it will only keep the
+    genotypes (GT).
     """
     header = _header_from_vcf(vcf_path, gzipped=vcf_path.endswith('.gz'))
 
@@ -37,10 +34,9 @@ def vcf_to_dataframe(vcf_path, keep_format_data=False, keep_samples=[]):
 
     usecols = header[:9] + keep_samples
 
-    df = pd.read_table(vcf_path, comment='#', names=header, sep='\s+',
-                       low_memory=False, usecols=usecols)
-    df.rename(columns={'#CHROM': 'CHROM'}, inplace=True)
-    df.rename(columns={col: col.lower() for col in df.columns[:9]}, inplace=True)
+    df = pd.read_table(vcf_path, comment='#', names=header, low_memory=False,
+                       usecols=usecols)
+    df = df.rename(columns={col: col.lower() for col in df.columns[:9]})
 
     if keep_samples and not keep_format_data:
         _extract_genos_and_make_them_categorical(df)
@@ -52,16 +48,6 @@ def vcf_to_dataframe(vcf_path, keep_format_data=False, keep_samples=[]):
 
     return df
 
-def _extract_genos_and_make_them_categorical(df):
-    """Extract the genotypes from the format fields of the VCF and make them
-    categorical. It changes the input DataFrame!"""
-    # Genotype columns range from the 10th to the last one
-    df.iloc[:, 9:] = df.iloc[:, 9:].applymap(_extract_genotype)
-
-    # Cast genotypes as category since it's MUCH more memory efficient
-    df.iloc[:, 9:] = df.iloc[:, 9:].apply(
-            lambda series: series.astype('category'))
-
 def _header_from_vcf(vcf_path, gzipped=False):
     """Read the header from a VCF file and return the found field names. It
     will gunzip on the fly if the path ends with '.gz' or if gzipped is set."""
@@ -72,11 +58,21 @@ def _header_from_vcf(vcf_path, gzipped=False):
             if isinstance(line, bytes):
                 line = line.decode('utf-8')
             if line.startswith('#CHROM'):
-                return line.strip().split('\t')
+                return line.strip().replace('#CHROM', 'CHROM').split('\t')
+
+def _extract_genos_and_make_them_categorical(df):
+    """Extract the genotypes from the format fields of the VCF and make them
+    categorical. It changes the input DataFrame!"""
+    # Genotype columns range from the 10th to the last one
+    df.iloc[:, 9:] = df.iloc[:, 9:].applymap(_extract_genotype)
+
+    # Cast genotypes as category since it's MUCH more memory efficient
+    df.iloc[:, 9:] = df.iloc[:, 9:].apply(
+            lambda series: series.astype('category'))
 
 def _extract_genotype(geno_field):
     """Extract the genotype from a format field."""
-    # Assume the genotype is the first format field, raise if it's not.
+    # Assume the genotype is the first format field and raise if it's not
     geno = geno_field.split(':')[0]
     if not GENO_REGEX.search(geno):
         raise ValueError('"{}" does not look like a genotype'.format(geno))
--- tests/test_vcf_to_dataframe.py
+++ tests/test_vcf_to_dataframe.py
@@ -3,7 +3,6 @@ import re
 import gzip
 
 import pytest
-from pytest import raises
 
 from anotamela import vcf_to_dataframe
 
@@ -18,8 +17,9 @@ def _test_vcf_to_dataframe(filename, gzipped):
     vcf_path = _test_file(filename)
 
     df = vcf_to_dataframe(vcf_path)
-    assert df.shape == (1742, 9)
+    sample_id = 'HG00096'
     assert len(df['chrom'].cat.categories) == 5
+    assert sample_id not in df.columns
 
     # Read the file "manually" to compare the data
     if gzipped:
@@ -36,11 +36,10 @@ def _test_vcf_to_dataframe(filename, gzipped):
 
     assert seen_ids == set(df['id'].unique())
 
-    df = vcf_to_dataframe(vcf_path, keep_samples='HG00096')
-    assert 'HG00096' in df.columns
-    assert df.shape == (1742, 10)
+    df = vcf_to_dataframe(vcf_path, keep_samples=sample_id)
+    assert sample_id in df.columns
 
-    with raises(ValueError):
+    with pytest.raises(ValueError):
         vcf_to_dataframe(vcf_path, keep_samples='non existent')
 
 def test_vcf_to_dataframe_gzipped():
