d973659dc57ab6b0b4757c357783ee8d3c63ff36
Juan Manuel Berros <juanmaberros@gmail.com>
2016-11-29 17:27:30 -0300

    WIP cache keys and ids

--- anotamela/cache/redis_cache.py
+++ anotamela/cache/redis_cache.py
@@ -11,16 +11,27 @@ logger = logging.getLogger(__name__)
 class RedisCache(Cache):
     def __init__(self, host='localhost', port=6379, db=0):
         self.client = redis.StrictRedis(host=host, port=port, db=db)
-        self.name = self.__class__.__name__
+        self.connection_data = self.client.connection_pool.connection_kwargs
 
-        _connection_data = {**self.client.connection_pool.connection_kwargs,
-                            'name': self.name}
-        logger.info('{name} connected to Redis cache: {host}:{port} '
-                    'db={db}'.format(**_connection_data))
+        logger.info('Connected to Redis @{host}:{port} db={db}'.format(
+                **self.connection_data))
 
-    def _client_set(self, info_dict):
+    def __repr__(self):
+        rep = '{name}(host={host}, port={port}, db={db})'
+        return rep.format({**self.connection_data,
+                           'name': self.__class__.__name__})
+
+    def _client_set(self, data_to_cache, namespace):
+        # Transform the ids to keys prefixing them with the given namespace
+        data_to_cache = {'{}:{}'.format(namespace, id_): value
+                         for id_, value in data_to_cache.items()}
         self.client.mset(data_to_cache)
 
-    def _client_get(self, keys):
-        return {key: value for key, value in zip(keys, self.client.mget(keys))}
+    def _client_get(self, ids_, namespace):
+        # Transform the ids to keys prefixing them with the given namespace
+        ids_, self.client.mget(['{}:{}'])
+        cached_data = {key: value for key, value in zip(keys, self.client.mget(keys))}
+
+    def _id_to_key(id_):
+        pass ####
 
