1c4d94731c7e324a2fa11b04e5432f6881e3bc28
Juan Manuel Berros <juanmaberros@gmail.com>
2016-11-30 13:59:49 -0300

    handle unknown cache error

--- anotamela/cache/cache.py
+++ anotamela/cache/cache.py
@@ -8,24 +8,18 @@ logger = logging.getLogger(__name__)
 class Cache:
     """
     Base class for caching that provides a common logic to set and retrieve
-    group of ids in the form of info dictionaries. Particularly, it will deal
-    with serializing and deserializing data during dump and load.
+    group of ids in the form of dictionaries. Particularly, it will deal with
+    serializing and deserializing data during dump and load, if needed.
 
     Classes inheriting from this class should implement the methods:
 
-        - _client_get(ids)
+        - _client_get(ids)  # => info_dict { id1: val1, id2: val2 ... }
         - _client_set(info_dict)
 
     """
-    def _client_get(ids, namespace):
-        raise NotImplementedError
-
-    def _client_set(data_to_cache, namespace):
-        raise NotImplementedError
-
     def get(self, ids, namespace=None, verbose=True):
         """
-        Get a list of ids data from cache. Returns an info dict with the form:
+        Get data for a list of ids from cache. Returns an info dict like:
 
         {
             id1: raw response of the service for id 1,
@@ -33,8 +27,10 @@ class Cache:
             ...
         }
 
-        The namespace will be used as a tablename or id prefix according to
-        each particular Cache class. Dicts and lists will be json-loaded.
+        During retrieval, the <namespace> will be used as a tablename or id
+        prefix according to each particular Cache class.
+
+        Dicts and lists will be json-loaded.
         """
         cached_data = self._client_get(ids, namespace=namespace)
         info_dict = {k: v for k, v in cached_data.items() if v}
@@ -84,4 +80,10 @@ class Cache:
             return json.loads(decoded)
         except ValueError:  # Not valid JSON, leave as is
             return decoded
+
+    def _client_get(ids, namespace):
+        raise NotImplementedError
+
+    def _client_set(data_to_cache, namespace):
+        raise NotImplementedError
 
