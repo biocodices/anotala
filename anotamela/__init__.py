1cbd5a9f9af93790a031b8465cc2d78b26ad8efd
Juan Manuel Berros <juanmaberros@gmail.com>
2016-11-30 13:13:49 -0300

    set WARNING loglevel for requests and urllib3

--- anotamela/__init__.py
+++ anotamela/__init__.py
@@ -1,4 +1,6 @@
 from os import environ
+import logging
+
 import coloredlogs
 
 from .annotators import DbSNPAnnotator
@@ -7,4 +9,7 @@ from .annotators import DbSNPAnnotator
 coloredlogs.DEFAULT_LOG_FORMAT = \
         '[@%(hostname)s %(asctime)s] %(levelname)s: %(message)s'
 coloredlogs.install(level=environ.get('LOGLEVEL') or 'INFO')
+
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
 
