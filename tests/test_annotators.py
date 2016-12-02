f15da10e8a996051e15f1b67ef06804b3be10cce
Juan Manuel Berros <juanmaberros@gmail.com>
2016-12-02 01:15:24 -0300

    WIP tests for many annotators

--- tests/test_annotators.py
+++ tests/test_annotators.py
@@ -1,12 +1,20 @@
 import pytest
 
-from anotamela.annotators import DbSNPAnnotator
+from anotamela.annotators import (
+        DbsnpWebAnnotator,
+        DbsnpEntrezAnnotator,
+    )
 
 
-def test_dbsnp_annotator():
-    ids_to_annotate = 'rs268 rs123'.split()
+ids_to_annotate = 'rs268 rs123'.split()
 
-    annotator = DbSNPAnnotator(cache='mock_cache')
+@pytest.mark.parametrize('annotator_class', [
+        DbsnpWebAnnotator,
+        DbsnpEntrezAnnotator
+    ])
+
+def test_generic_annotator(annotator_class):
+    annotator = annotator_class(cache='mock_cache')
     info_dict = annotator.annotate(ids=ids_to_annotate)
 
     assert all(id_ in info_dict for id_ in ids_to_annotate)
