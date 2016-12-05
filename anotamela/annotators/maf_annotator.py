a485731236e348aa5faab2f32df27e4dcfe82ea6
Juan Manuel Berros <juanmaberros@gmail.com>
2016-12-05 00:20:30 -0300

    MafAnnotator with tests passing

---
+++ anotamela/annotators/maf_annotator.py
@@ -0,0 +1,26 @@
+from anotamela.annotators import AnnotatorWithCache, MyVariantAnnotator
+
+
+class MafAnnotator(MyVariantAnnotator, AnnotatorWithCache):
+    SOURCE_NAME = 'maf'
+    SCOPES = 'dbnsfp.rsid'
+    FIELDS = 'dbsnp.gmaf dbnsfp.1000gp3 dbnsfp.exac dbnsfp.esp6500'.split()
+
+    @staticmethod
+    def _parse_annotation(raw_annotation):
+        dicts = (
+            ('1000gp3', raw_annotation.get('dbsnp', {})),
+            ('1000gp3', raw_annotation.get('dbnsfp', {}).get('1000gp3', {})),
+            ('esp6500', raw_annotation.get('dbnsfp', {}).get('esp6500', {})),
+            ('exac', raw_annotation.get('dbnsfp', {}).get('exac', {}))
+        )
+
+        annotation = {}
+        for source_name, dic in dicts:
+            for key, value in dic.items():
+                compound_key = '{}_{}'.format(source_name, key)
+                annotation[compound_key] = round(value, 4)
+
+        return {k: v for k, v in annotation.items()
+                if not k.endswith('_ac')}
+
