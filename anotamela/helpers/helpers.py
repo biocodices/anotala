695029553fcbbad7b8a8c8cd051492712a47e7b1
Juan Manuel Berros <juanmaberros@gmail.com>
2016-12-09 22:55:27 -0300

    PubmedAnnotator

--- anotamela/helpers/helpers.py
+++ anotamela/helpers/helpers.py
@@ -46,3 +46,55 @@ def camel_to_snake(s):
     """Convert a CamelCase string to a snake_case string."""
     return re.sub("([A-Z])", "_\\1", s).lower().lstrip('_')
 
+
+def access_deep_keys(keys, dic, ignore_key_errors=False):
+    """
+    Given a dictionary with maybe nested dictionaries in it, return a
+    1-level flat new dictionary that includes the keys/values passed in keys.
+    If a key has a dot in it (e.g. 'Article.Abstract'), it will be treated
+    as dic['Article']['Abstract'] and the new dicitonary will have a compound
+    key new_dict['Article.Abstract'].
+
+    Usage:
+
+        > dic = {'foo' 1,
+                 'bar': {'baz': 2,
+                         'qux': {'ham': 3}}}
+        > access_deep_keys(['foo', 'bar.baz', 'bar.qux.ham'], dic)
+        # => {'bar.baz': 2, 'bar.qux.ham': 3, 'foo': 1}
+
+    If ignore_key_errors=True, any failing keys will be ignored and you will
+    get the result of the successful keys.
+    """
+    new_dic = {}
+    for queried_key in keys:
+
+        # A key with a dot means we have to go deeper
+        if '.' in queried_key:
+            key, deep_key = queried_key.split('.', 1)
+            try:
+                deep_dic = dic[key]
+            except KeyError:
+                if not ignore_key_errors:
+                    raise
+            else:
+                if not isinstance(deep_dic, dict):
+                    msg = "You ask for '{}' but '{}' is not a dictionary"
+                    raise ValueError(msg.format(queried_key, key))
+
+                deep_values = access_deep_keys(
+                        [deep_key], dic[key],
+                        ignore_key_errors=ignore_key_errors
+                    )
+                new_dic[queried_key] = deep_values.get(deep_key)
+
+        # A regular key with no dots in it
+        else:
+            try:
+                new_dic[queried_key] = dic[queried_key]
+            except KeyError:
+                if not ignore_key_errors:
+                    raise
+
+    return new_dic
+
