501d5a516c6f5cdd4d36d39da35f852ee2993b19
Juan Manuel Berros <juanmaberros@gmail.com>
2016-12-21 12:31:51 -0300

    DictCache for testing and script to update testing pickles

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,29 +1,6 @@
-from collections import defaultdict
 from os.path import join, dirname
-import json
-
-from anotamela.cache import Cache
 
 
 def get_test_file(filename):
     return join(dirname(__file__), 'files', filename)
-
-
-class MockCache(Cache):
-    def __init__(self):
-        self.name = 'Mock Cache'
-        self.storage = defaultdict(dict)
-
-    def _client_set(self, info_dict, namespace, save_as_json):
-        table = self.storage[namespace]
-        if save_as_json:
-            info_dict = {k: json.dumps(v) for k, v in info_dict.items()}
-        table.update(info_dict)
-
-    def _client_get(self, keys, namespace, load_as_json):
-        table = self.storage[namespace]
-        info_dict = {k: table[k] for k in keys if k in table}
-        if load_as_json:
-            info_dict = {k: json.loads(v) for k, v in info_dict.items()}
-        return info_dict
 
