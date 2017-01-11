5f6810113bd4d5f99a286ebdc44b240be6461e3b
Juan Manuel Berros <juanmaberros@gmail.com>
2017-01-11 16:01:37 -0300

    PR tweaks

--- tests/test_annotators.py
+++ tests/test_annotators.py
@@ -164,7 +164,7 @@ def test_annotator(proxies, annotator_class, params):
     # Test annotate_one
     assert result[id_] == annotator.annotate_one(id_, use_web=False)
 
-    # Test the annotators can pass the raw data instead of the parsed info
+    # Test the annotators can return the raw data instead of the parsed info
     if hasattr(annotator, '_parse_annotation'):
         raw = annotator.annotate(id_, use_web=False, parse=False)
         assert raw[id_]
