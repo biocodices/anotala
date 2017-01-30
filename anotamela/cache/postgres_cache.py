56aaeb508b8657129693905f23dba9ca93688919
Juan Manuel Berros <juanmaberros@gmail.com>
2017-01-30 19:09:09 -0300

    WIP: mysql cache

--- anotamela/cache/postgres_cache.py
+++ anotamela/cache/postgres_cache.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 class PostgresCache(Cache):
     CREDS_FILE = '~/.postgres_credentials.yml'
+    JSON_TYPE = JSONB
 
     def __init__(self, credentials_filepath=CREDS_FILE):
         """
@@ -24,7 +25,8 @@ class PostgresCache(Cache):
         self._creds_filepath = credentials_filepath
         self._credentials = self._read_credentials(credentials_filepath)
         self._connect(self._credentials)
-        logger.info('Connected to PostgreSQL ({!r})'.format(self.engine.url))
+        logger.info('Connected to {} ({!r})'.format(self.__class__.__name__,
+                                                    self.engine.url))
         self.tables = {}
 
     def __repr__(self):
@@ -35,10 +37,10 @@ class PostgresCache(Cache):
     def _client_get(self, ids, namespace, load_as_json):
         # The argument load_as_json is only relevant the first time an
         # annotator is used, in self._get_table(), to create the table with a
-        # JSONB field type. After this, the psql client automatically deals with
+        # JSON field type. After this, the psql client automatically deals with
         # [de]serialization of JSON, so the setting and retrieval of data is
         # the same wether the field is JSON or not.
-        table = self._get_table(namespace, json_type=load_as_json)
+        table = self._get_table(namespace, as_json=load_as_json)
         select_query = table.select().where(table.c.id.in_(ids))
         query_result = self.connection.execute(select_query)
         return {row['id']: row['annotation'] for row in query_result}
@@ -46,10 +48,10 @@ class PostgresCache(Cache):
     def _client_set(self, info_dict, namespace, save_as_json):
         # The argument load_as_json is only relevant the first time an
         # annotator is used, in self._get_table(), to create the table with a
-        # JSONB field type. After this, the psql client autoatically deals with
+        # JSON field type. After this, the psql client autoatically deals with
         # [de]serialization of JSON, so the setting and retrieval of data is
         # the same wether the field is JSON or not.
-        table = self._get_table(namespace, json_type=save_as_json)
+        table = self._get_table(namespace, as_json=save_as_json)
         ids_to_remove = info_dict.keys()
         self._client_del(ids_to_remove, namespace)
         new_annotations = [{'id': id_, 'annotation': ann}
@@ -67,14 +69,14 @@ class PostgresCache(Cache):
         self.engine = create_engine(url)
         self.connection = self.engine.connect()
 
-    def _get_table(self, tablename, json_type=False):
+    def _get_table(self, tablename, as_json=False):
         """Get a SQLAlchemy Table for the given tablename."""
         if tablename not in self.tables:
-            self.tables[tablename] = self._create_table(tablename, json_type)
+            self.tables[tablename] = self._create_table(tablename, as_json)
 
         return self.tables[tablename]
 
-    def _create_table(self, tablename, json_type):
+    def _create_table(self, tablename, as_json):
         """
         Create a SQLAlchemy Table for the given tablename. All tables created
         have the same columns: id, annotation, synonyms, and last_updated.
@@ -84,11 +86,11 @@ class PostgresCache(Cache):
         Returns a sqlalchemy.Table instance.
         """
         metadata = MetaData()
-        annotation_column_type = JSONB if json_type else String
+        annotation_column_type = self.JSON_TYPE if as_json else String
         table = Table(tablename, metadata,
                       Column('id', String(60), primary_key=True),
                       Column('annotation', annotation_column_type),
-                      Column('synonyms', JSONB),  # Just a list of IDs
+                      Column('synonyms', self.JSON_TYPE),  # Just a list of IDs
                       Column('last_updated', DateTime(timezone=True),
                              default=datetime.now, nullable=False))
         metadata.create_all(self.engine)
