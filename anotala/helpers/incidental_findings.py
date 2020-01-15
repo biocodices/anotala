cb3e9a8e35c0c6906dff1ca8e5a8aaa4bc3300cf
Juan Manuel Berros <juanmaberros@gmail.com>
2020-01-15 13:15:40 -0300

    updated get incidental findings parse logic

--- anotala/helpers/incidental_findings.py
+++ anotala/helpers/incidental_findings.py
@@ -15,40 +15,56 @@ def get_omim_incidental_genes_and_phenos():
     fn = join(gettempdir(), 'ACMG_incidental_genes_{}.csv'.format(date_str))
 
     if not isfile(fn):
-        tables = pd.read_html('https://www.ncbi.nlm.nih.gov/clinvar/docs/acmg/')
+        url = 'https://www.ncbi.nlm.nih.gov/clinvar/docs/acmg/'
+        print(f'ACMG file not cached for this month. Fetching from: {url}')
+        tables = pd.read_html(url)
+
         assert len(tables) == 1  # Make sure there's only one table in the page
         df = tables[0]
 
-        df.columns = df.iloc[0]
-        df = df.drop(0).reset_index(drop=True)
-        df = fix_df(df)
-
-        name = re.compile(r'(.+) ?\(')
-        mim_id = re.compile(r'.+ ?\(MIM (\d+)\)')
-
-        mapping = {'Disease name and MIM number': 'phenotype',
-                   'Gene via GTR': 'gene'}
-        for old_field, new_field in mapping.items():
-            df[new_field] = df[old_field].str.extract(name, expand=False)
-            df[new_field] = df[new_field].str.strip()
-            df[new_field + '_MIM'] = df[old_field].str.extract(mim_id,
-                                                               expand=False)
-            df = df.drop(old_field, axis=1)
-
-        no_gene = df['gene'].isnull()
-        df.loc[no_gene, 'gene'] = \
-            df.loc[no_gene, 'MedGen'].str.extract(r'MedGen\s+(\w+) \(', expand=False)
-        df.loc[no_gene, 'gene_MIM'] = \
-            df.loc[no_gene, 'MedGen'].str.extract(r'\(MIM (\d+)\)', expand=False)
-
-        df = df.drop(['MedGen', 'Variations that maybe pathogenic'], axis=1)
-
+        try:
+            df = _parse_incidental_findings(df)
+        except Exception:
+            unparsed_fn = fn.replace('.csv', '.unparsed.csv')
+            df.to_csv(unparsed_fn)
+            print(f'Failed parsing. Dumped the unparsed CSV in:\n{fn}')
+            print(df.head())
+            raise
         df.to_csv(fn, index=False)
 
     return pd.read_csv(fn, dtype={'gene_MIM': str, 'phenotype_MIM': str})
 
 
-def fix_df(df):
+def _parse_incidental_findings(df):
+    # Seems this is not necessary anymore:
+    # df = _use_first_row_as_header(df)
+    df = fix_df_colspan_issues(df)
+
+    name = re.compile(r'(.+) ?\(')
+    mim_id = re.compile(r'.+ ?\(MIM (\d+)\)')
+
+    mapping = {'Disease name and MIM number': 'phenotype',
+               'Gene via GTR': 'gene'}
+    for old_field, new_field in mapping.items():
+        df[new_field] = df[old_field].str.extract(name, expand=False)
+        df[new_field] = df[new_field].str.strip()
+        df[new_field + '_MIM'] = df[old_field].str.extract(mim_id,
+                                                           expand=False)
+        df = df.drop(old_field, axis=1)
+
+    no_gene = df['gene'].isnull()
+    df.loc[no_gene, 'gene'] = \
+        df.loc[no_gene, 'MedGen'].str.extract(r'MedGen\s+(\w+) \(',
+                                              expand=False)
+    df.loc[no_gene, 'gene_MIM'] = \
+        df.loc[no_gene, 'MedGen'].str.extract(r'\(MIM (\d+)\)', expand=False)
+
+    df = df.drop(['MedGen', 'Variations that maybe pathogenic'], axis=1)
+    return df
+
+
+
+def fix_df_colspan_issues(df):
     # Fix some missing values because colspan > 1
     # FIXME: this function is ugly, it was a quick hack that stuck. Rewrite.
     last_row = None
@@ -99,3 +115,8 @@ def is_incidental_pheno(pheno_mim_id):
     df = get_omim_incidental_genes_and_phenos()
     return str(pheno_mim_id) in df['phenotype_MIM'].values
 
+
+def _use_first_row_as_header(df):
+    df.columns = df.iloc[0]
+    df = df.drop(0).reset_index(drop=True)
+    return df
